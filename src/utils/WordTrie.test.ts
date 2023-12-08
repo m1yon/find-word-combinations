014ef053bb7a63947750b0ee5aaaf4734efb502a
Michael Lyon <mlyon360@gmail.com>
2023-12-07 19:33:38 -0800

    add a single word and seperated tree tests for WordTrie

--- src/utils/WordTrie.test.ts
+++ src/utils/WordTrie.test.ts
@@ -1,9 +1,24 @@
 import { expect, it } from "bun:test";
 import { WordTrie } from "./WordTrie";
 
+it("generates a single node correctly", () => {
+  const root = new WordTrie({ value: "a" });
+
+  expect(root).toMatchSnapshot();
+});
+
 it("generates the correct tree given a single word", () => {
   const root = new WordTrie({ value: "" });
   root.addWord("great");
+
+  expect(root).toMatchSnapshot();
+});
+
+it("generates the correct tree given multiple words that do not connect", () => {
+  const root = new WordTrie({ value: "" });
+  root.addWord("red");
+  root.addWord("tree");
+  root.addWord("far");
 
   expect(root).toMatchSnapshot();
 });
